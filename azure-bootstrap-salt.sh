4dcd7abddd785367e95a10726c8ff762da28136f
Kenny Johansson <wirehell@gmail.com>
2016-03-04 10:38:13 +0100

    Create azure-bootstrap-salt.sh

---
+++ azure-bootstrap-salt.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Wrapper to bootstrap Salt and add settings to keep connection alive
+
+/bin/sh bootstrap.sh $@
+cat << EOF > /etc/salt/minion.d/42-keepalive.conf
+ping_interval: 1
+tcp_keepalive: True
+tcp_keepalive_idle: 120
+EOF
+/usr/sbin/service salt-minion restart
